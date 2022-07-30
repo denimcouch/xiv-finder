abdc0e7da1dcb49bf62ec868a728f91d6762fdc3
denimcouch <alexmata92@ymail.com>
2022-07-30 14:24:34 -0500

    set up navbar layout, added about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Flex } from '@chakra-ui/react'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
+import About from './pages/About'
 import Character from './pages/Character'
 
 const App = () => {
   return (
     <Router>
       <Flex
+        className='xivApp'
         direction='column'
         h='100vh'
         justify='space-between'
@@ -17,6 +19,7 @@ const App = () => {
         <main>
           <Routes>
             <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
             <Route path='/character/:name' element={<Character />} />
           </Routes>
         </main>
