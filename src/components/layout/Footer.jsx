fb6f876c92174ff84031928ea8d903bf494e123b
denimcouch <alexmata92@ymail.com>
2022-07-30 14:29:56 -0500

    footer layout is fine

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,15 @@
+import { Container, Flex } from '@chakra-ui/react'
+
 const Footer = () => {
+  const footerYear = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <footer className='xivFooter'>
+      <Container maxW='container.lg' py='2rem'>
+        <Flex justify='center'>
+          <p>Copyright &copy; {footerYear} All rights reserved</p>
+        </Flex>
+      </Container>
+    </footer>
   )
 }
 
